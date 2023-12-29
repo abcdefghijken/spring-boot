d430a979b0150fbfc5f269eceb2551b8b87da495
Ken Amadeus <k-en@live.com.sg>
2023-12-29 22:33:50 +0800

    spring beans

--- 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/ken/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/ken/springcoredemo/rest/DemoController.java
@@ -11,7 +11,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public DemoController(@Qualifier("swimCoach") Coach theCoach) {
+    public DemoController(@Qualifier("aquatic") Coach theCoach) {
         System.out.println("In constructor: " + getClass().getSimpleName());
         this.myCoach = theCoach;
     }
